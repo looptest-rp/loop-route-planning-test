fbb046471c3f032e55e51edd7c09f6a52ed7d8aa
looptest-rp <128223181+looptest-rp@users.noreply.github.com>
2023-03-18 14:46:57 +0200

    Add files via upload

--- model_app.py
+++ model_app.py
@@ -4,41 +4,54 @@
 # In[ ]:
 
 
-import streamlit as st
 import pandas as pd
 import folium
-from streamlit_folium import st_folium
-from model_with_map import cluster_data
+from k_means_constrained import KMeansConstrained
 
-# Add a title to the app
-st.title("Route Planning App")
-
-# Create a file uploader component in Streamlit
-file = st.file_uploader("Upload a CSV file with orders latitude and longitude fields")
-
-# Add a sidebar to the app
-st.sidebar.title("Input Parameters")
-
-# Accept user input for n_clusters and max_weight
-n_clusters = st.sidebar.text_input("Number of clusters", value="50")
-max_weight = st.sidebar.text_input("Maximum stops per cluster", value="28")
-
-# If a file is uploaded, cluster the data and display the results
-if file is not None:
-    # Convert the input values to integers
-    n_clusters = int(n_clusters)
-    max_weight = int(max_weight)
-
-    # Call the cluster_data function with the user input arguments
-    data, m = cluster_data(file, n_clusters, max_weight)
+def cluster_data(file, n_clusters, max_weight):
+    # Read the CSV file into a Pandas dataframe
+    data = pd.read_csv(file)
     
-    # Display the data with the cluster labels
-    st.write("Clustered Data:")
-    st.write(data)
+    if 'Weight' in data.columns:
+        data = data.loc[data.index.repeat(data['Weight'])].reset_index(drop=True)
+        data['Weight'] = 1
+    else:
+        data['Weight'] = 1
+
+    # Apply the KMeansConstrained algorithm to the dataframe
+    km_cons = KMeansConstrained(n_clusters=n_clusters,
+                                init='k-means++',
+                                size_max=max_weight,
+                                random_state=42,
+                                n_jobs=-1)
+    y_predicted = km_cons.fit_predict(data[['longitude', 'latitude']])
     
-    # Display the folium map
-    st.write("Cluster Map:")
-    st_folium(m, width=1000)
-else:
-    st.write("Please upload a file to begin clustering.")
+    # Add the cluster labels to the dataframe
+    data['cluster'] = y_predicted + 1
+    
+    # Create a list of colors for the clusters
+    colors = ['red', 'blue', 'green', 'purple', 'orange', 'darkred', 'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue', 'darkpurple', 'pink', 'lightblue', 'lightgreen', 'gray', 'black', 'lightgray']
+    colors *= n_clusters // len(colors) + 1
+    
+    # Create a folium map object centered on the mean of the delivery locations
+    m = folium.Map(location=[data['latitude'].mean(), data['longitude'].mean()], zoom_start=12)
+    
+    # Add cluster markers and lines to the map
+    for i in range(1, n_clusters+1):
+        cluster_data = data[data['cluster'] == i]
+        color = colors[i-1]
+        cluster_points = []
+        for _, row in cluster_data.iterrows():
+            # Add marker for each point
+            folium.Marker(location=[row['latitude'], row['longitude']],
+                          icon=folium.Icon(color=color),
+                          popup=f"Cluster {i}").add_to(m)
+            # Add point to list of cluster points
+            cluster_points.append([row['latitude'], row['longitude']])
+        # Add polyline connecting the cluster points
+        folium.PolyLine(locations=cluster_points,
+                        color=color).add_to(m)
+    
+    # Return the dataframe with the cluster labels and the folium map object
+    return data, m
 
